659bd672c34389358a4b59834ac57d3c691ae38f
Q-honoka <ncr2419gp0006@edu.nca.ac.jp>
2026-04-15 11:06:36 +0900

    テスト用に計算結果を返す関数を作成した

---
+++ JoyconNativePlugin/JoyconNativePlugin.cpp
@@ -0,0 +1,10 @@
+﻿#include "pch.h"
+#include "JoyconNativePlugin.h"
+
+// 引数で与えられた２つの数を足した値を返す
+int Add(
+	const int a,
+	const int b)
+{
+	return a + b;
+}
---
+++ JoyconNativePlugin/JoyconNativePlugin.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#ifdef JOYCONNATIVEPLUGIN_EXPORTS
+#define JOYCONNATIVEPLUGIN_API __declspec(dllexport)
+#else
+#define JOYCONNATIVEPLUGIN_API __declspec(dllimport)
+#endif // JOYCONNATIVEPLUGIN_EXPORTS
+
+
+// 引数で与えられた２つの数を足した値を返す（テスト用）
+extern "C" JOYCONNATIVEPLUGIN_API int Add(
+	const int a, const int b);
