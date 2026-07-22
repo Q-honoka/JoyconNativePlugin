cb68459060a9257c81e090d0e3622926a89c56ce
Q-honoka <ncr2419gp0006@edu.nca.ac.jp>
2026-07-22 10:35:24 +0900

    アップデートスレッドを起動させるように戻した

--- JoyconNativePlugin/JoyConManager.cpp
+++ JoyconNativePlugin/JoyConManager.cpp
@@ -73,7 +73,7 @@ void JoyConManager::Initialize()
 
 	// スレッドを起動する
 	isRunning = true;
-	//updateThread = std::thread(&JoyConManager::Update, this);
+	updateThread = std::thread(&JoyConManager::Update, this);
 
 	completeInit = true;
 }
